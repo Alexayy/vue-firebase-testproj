e27ba344c18c40c0dd88b29953c0141be8230c8f
Alexayy <aleksa.cakic@gmail.com>
2019-07-17 05:46:55 +0200

    initial commit

---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyABVH7rpj5z3xpPicoK4FDH_cbdEHoowS8",
+    authDomain: "ninja-smoothies-e2d17.firebaseapp.com",
+    databaseURL: "https://ninja-smoothies-e2d17.firebaseio.com",
+    projectId: "ninja-smoothies-e2d17",
+    storageBucket: "ninja-smoothies-e2d17.appspot.com",
+    messagingSenderId: "129588412468",
+    appId: "1:129588412468:web:48fc81e09e3551cc"
+  };
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  export default firebaseApp.firestore()
