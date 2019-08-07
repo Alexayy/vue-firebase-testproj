f8085dc8d4b6ca926deee50da0fe37d84715da5c
Alexayy <aleksa.cakic@gmail.com>
2019-08-07 21:32:18 +0200

    deploy fixed

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,10 +3,10 @@ import firestore from 'firebase/firestore'
 
 var firebaseConfig = {
     apiKey: "AIzaSyABVH7rpj5z3xpPicoK4FDH_cbdEHoowS8",
-    authDomain: "ninja-members-e2d17.firebaseapp.com",
+    authDomain: "ninja-smoothies-e2d17.firebaseapp.com",
     databaseURL: "https://ninja-smoothies-e2d17.firebaseio.com",
-    projectId: "ninja-members-e2d17",
-    storageBucket: "ninja-members-e2d17.appspot.com",
+    projectId: "ninja-smoothies-e2d17",
+    storageBucket: "ninja-smoothies-e2d17.appspot.com",
     messagingSenderId: "129588412468",
     appId: "1:129588412468:web:48fc81e09e3551cc"
   };
